11521fa6fda0365a1fec392fb891141ab6429645
Akande George <48625249+Akande-George@users.noreply.github.com>
2023-11-25 11:15:50 +0100

    hero image

--- components/contents/Home/Hero.vue
+++ components/contents/Home/Hero.vue
@@ -1,6 +1,9 @@
 <template>
-  <section class="pt-14">
+  <section class="pt-2 md:pt-14 pb-14">
     <div class="grid grid-cols-1 lg:grid-cols-2">
+      <div class="block lg:hidden mb-4 md:mb-10">
+        <img src="/images/heroImage2.png" class="w-full rounded-[4px]" alt="" />
+      </div>
       <div>
         <div
           class="text-3xl md:text-4xl lg:text-6xl app-font-Semibold md:w-[90%] leading-[118.4%]"
@@ -8,14 +11,14 @@
           Redefine your travel experience with Comfort, Speed & Style
         </div>
       </div>
-      <div class="pt-6">
+      <div class="pt-2 md:pt-6">
         <div class="text-[18px] md:text-[20px]">
           Welcome to Ava, where you are winged for your comfort! We are thrilled
           to offer exclusive rentals on private jets worldwide tailored for
           those who value comfort, speed, and style. Serving Nigeria and
           Nigerians, we're here to redefine your travel experience.
         </div>
-        <div class="flex justify-start mt-6 space-x-3 md:space-x-6">
+        <div class="flex justify-start mt-3 md:mt-6 space-x-3 md:space-x-6">
           <button
             class="btn bg-primary text-sm md:text-md py-2 px-3 lg:py-6 lg:px-12 rounded-[4px]"
           >
@@ -29,7 +32,7 @@
         </div>
       </div>
     </div>
-    <div class="my-14">
+    <div class="my-14 hidden lg:block">
       <img src="/images/heroImage.png" class="w-full rounded-[4px]" alt="" />
     </div>
   </section>
