446c44465661edecbee6217e74796d4915985690
Akande George <48625249+Akande-George@users.noreply.github.com>
2023-11-27 06:34:51 +0100

    fixed responsiveness

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,10 +7,9 @@ export default defineNuxtConfig({
         {
           src: "https://whatsform.com/launcher.js",
           id: "wf-widget",
-          body: true,
           async: true,
           defer: true,
-          "data-id": "_ExmQL",
+          "data-id": "J4wsW1",
           "data-message": "Message on WhatsApp",
         },
       ],
