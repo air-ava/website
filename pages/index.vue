7e7a0a1327129e47094f0f217cd3987969c85d3e
Akande George <48625249+Akande-George@users.noreply.github.com>
2023-11-30 10:06:12 +0100

    seo image fixed

--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,16 @@
           to offer exclusive rentals on private jets worldwide tailored for
           those who value comfort...."
       />
-      <OgImage v-bind="ogImageOptions" />
     </Head>
-    <Content />
+    <ContentsHome />
   </div>
 </template>
 
-<script>
-import Content from "@/components/contents/Home";
-export default {
-  components: {
-    Content,
-  },
-};
+<script setup lang="ts">
+useSeoMeta({
+  ogImage:
+    "https://res.cloudinary.com/godfadatun/image/upload/v1701054655/airava/IMG-20231122-WA0012_1_qpb864.png",
+});
 </script>
 
 <style></style>
